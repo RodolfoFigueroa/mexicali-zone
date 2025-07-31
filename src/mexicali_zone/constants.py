6cd2a10ada41c6e083b5c5a0adcc3e71ad8ea29e
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2025-07-31 14:17:36 -0600

    Add notebooks

--- src/mexicali_zone/constants.py
+++ src/mexicali_zone/constants.py
@@ -1,8 +1,8 @@
 BOUNDS_FILE = "bounds.fgb"
-CSV_DENUE = 'temp_data/conjunto_de_datos/denue_inegi_{0}_.csv'
-DENUE = 'https://www.inegi.org.mx/contenidos/masiva/denue/denue_{0}_csv.zip'
+CSV_DENUE = "temp_data/conjunto_de_datos/denue_inegi_{0}_.csv"
+DENUE = "https://www.inegi.org.mx/contenidos/masiva/denue/denue_{0}_csv.zip"
 ESTABLISHMENTS_FILE = "establishments.fgb"
-REGEX_PPL = r'([0-9]+) a ([0-9]+) personas'
+REGEX_PPL = r"([0-9]+) a ([0-9]+) personas"
 SECTORS_MAP = [
     {"range": [(11, 11)], "sector": "primario"},
     {"range": [(21, 33)], "sector": "industria"},
